2c906e3860f5fdd4784d0037027848fea7c2ab26
Ditrich79 <adk18@mail.ru>
2023-01-26 20:03:06 +0300

    stepik2601

---
+++ homeworks/stepik.py
@@ -0,0 +1,118 @@
+# def do_something():
+#   a = 1
+#   print(a)
+#
+# a = 0
+# do_something()
+# print(a)
+
+# a = 1
+#
+# def do_something():
+#   print(a)
+#
+# do_something()
+
+# def draw_triangle():
+#     for i in range(8):
+#         print(' ' * (15 - 1 - i) + '*' * (1 + i * 2))
+#
+#
+# draw_triangle()
+
+# def get_shipping_cost(quantity):
+#     cost = 1000
+#     for i in range(1, quantity):
+#         cost += 120
+#     return cost
+#
+#
+# n = int(input())
+#
+#
+# print(get_shipping_cost(n))
+
+# from math import factorial
+#
+# def compute_binom(n, k):
+#     bnom = factorial(n) / (factorial(k) * factorial(n - k))
+#     return bnom
+#
+#
+# n = int(input())
+# k = int(input())
+#
+#
+# print(compute_binom(n, k))
+
+# def number_to_words(num):
+#     zero_to_ninety_nine = ['один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять',
+#                            'десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать',
+#                            'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать', 'двадцать', ' ']
+#     zero_to_ninety_nine2 = ['двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят',
+#                             'девяносто']
+#     if num <= 20:
+#         return zero_to_ninety_nine[num - 1]
+#     else:
+#         return zero_to_ninety_nine2[num // 10 - 2] + ' ' + zero_to_ninety_nine[num % 10 - 1]
+#
+#
+# n = int(input())
+#
+#
+# print(number_to_words(n))
+
+# def get_month(language, number):
+#     lng_ru = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь',
+#               'декабрь']
+#
+#     lng_en = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
+#               'november', 'december']
+#     if language == 'en':
+#         return lng_en[num - 1]
+#     else:
+#         return lng_ru[num - 1]
+#
+#
+# lan = input()
+# num = int(input())
+#
+#
+# print(get_month(lan, num))
+
+
+# def is_magic(date):
+#     s = date.split('.')
+#     for i in range(len(s)):
+#         s[i] = int(s[i])
+#     md1 = s[2] % 100
+#     md2 = s[0] * s[1]
+#     if md1 == md2:
+#         return True
+#     else:
+#         return False
+#
+#
+# date = input()
+#
+#
+# print(is_magic(date))
+
+def is_pangram(text):
+    lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+           'v', 'w', 'x', 'y', 'z']
+    text = text.replace(' ', '')
+    text = text.lower()
+    for i in lst:
+        if i not in text:
+            return False
+        else:
+            return True
+
+
+text = input()
+
+
+print(is_pangram(text))
+
+
