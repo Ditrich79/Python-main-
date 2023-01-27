8e3df151700fac6c301dee992dcb66bc8b818c20
Ditrich79 <adk18@mail.ru>
2023-01-27 18:57:20 +0300

    stepik2601

--- homeworks/stepik.py
+++ homeworks/stepik.py
@@ -98,21 +98,106 @@
 #
 # print(is_magic(date))
 
-def is_pangram(text):
-    lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-           'v', 'w', 'x', 'y', 'z']
-    text = text.replace(' ', '')
-    text = text.lower()
-    for i in lst:
-        if i not in text:
-            return False
-        else:
-            return True
+# def is_pangram(text):
+#     lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+#            'v', 'w', 'x', 'y', 'z']
+#     text = text.replace(' ', '')
+#     text = text.lower()
+#     for i in lst:
+#         if i not in text:
+#             return False
+#         return True
+#
+#
+# text = input()
+#
+#
+# print(is_pangram(text))
 
+# list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+# list1[2][2].append(7000)
+#
+# print(list1)
 
-text = input()
+# list1 = ['a', 'b', ['c', ['d', 'e', ['f', 'g'], 'k'], 'l'], 'm', 'n']
+# sub_list = ['h', 'i', 'j']
+# list1[2][1][2].extend(sub_list)
+#
+# print(list1)
 
+# list1 = [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+# maximum = -1
+# for i in list1:
+#     if max(i) > maximum:
+#         maximum = max(i)
+# print(maximum)
 
-print(is_pangram(text))
+# list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+# for i in list1:
+#     i.reverse()
+# print(list1)
 
+# list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+# total = 0
+# counter = 0
+# for i in list1:
+#     total += sum(i)
+#     counter += len(i)
+# print(total / counter)
+
+# n, m = int(input()), int(input())
+#
+# my_list = [[0]*m for _ in range(n)]
+# print(my_list)
+
+# my_list = [[12, 221, 3], [41, 5, 633], [71, 8, 99]]
+#
+# maximum = my_list[0][0]
+# minimum = my_list[0][0]
+#
+# for row in my_list:
+#     maximum = max(row)
+#     minimum = min(row)
+#
+# print(maximum + minimum)
+
+# my_list = [[12, 221, 3], [41, 5, 633], [71, 8, 99]]
+#
+# maximum = my_list[0][0]
+# minimum = my_list[0][0]
+#
+# for row in my_list:
+#     if max(row) > maximum:
+#         maximum = max(row)
+#     if min(row) < minimum:
+#         minimum = min(row)
+#
+# print(maximum + minimum)
+
+# n = int(input())
+# list1 = [[j for j in range(1, n + 1)] for i in range(n)]
+# for row in list1:
+#     print(row)
+
+# n = int(input())
+# list1 = [[j for j in range(1, i + 1)] for i in range(1, n + 1)]
+# for row in list1:
+#     print(row)
+
+n = 5
+a = [[19, 21, 33, 78, 99],
+     [41, 53, 66, 98, 76],
+     [79, 80, 90, 60, 20],
+     [33, 11, 45, 67, 90],
+     [45, 67, 12, 98, 23]]
+
+maximum = -1
+minimum = 100
+
+for i in range(n):
+    if a[i][i] > maximum:
+        maximum = a[i][i]
+    if a[i][n - i - 1] < minimum:
+        minimum = a[i][n - i - 1]
+print(minimum + maximum)
 
