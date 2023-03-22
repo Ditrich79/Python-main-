e8eb8884cacd3ee5be4fee55e285cbbae3966fe2
Ditrich79 <adk18@mail.ru>
2023-03-22 22:13:04 +0300

    main

--- main.py
+++ main.py
@@ -6277,37 +6277,247 @@ import os.path
 # print(p1.__dict__)
 
 
-class Integer:
-    @classmethod
-    def verify_coord(cls, coord):
-        if not isinstance(coord, int):
-            raise TypeError(f"Координата {coord} должна быть целым числом")
+# class Integer:
+#     @classmethod
+#     def verify_coord(cls, coord):
+#         if not isinstance(coord, int):
+#             raise TypeError(f"Координата {coord} должна быть целым числом")
+#
+#     def __set_name__(self, owner, name):
+#         self.name = "_" + name
+#
+#     def __get__(self, instance, owner):
+#         # return instance.__dict__[self.name]
+#         return getattr(instance, self.name)
+#
+#     def __set__(self, instance, value):
+#         self.verify_coord(value)
+#         # instance.__dict__[self.name] = value
+#         setattr(instance, self.name, value)
+#
+#
+# class Point3D:
+#     x = Integer()
+#     y = Integer()
+#     z = Integer()
+#
+#     def __init__(self, x, y, z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#
+#
+# p1 = Point3D(1, 2, 3)
+# print(p1.y)
+# p1.z = 100
+# print(p1.__dict__)
 
-    def __set_name__(self, owner, name):
-        self.name = "_" + name
 
-    def __get__(self, instance, owner):
-        # return instance.__dict__[self.name]
-        return getattr(instance, self.name)
+# class Rectangle:
+#     def __init__(self, w, h):
+#         self.w = w
+#         self.h = h
+#
+#     def get_perimetr(self):
+#         return 2 * (self.w + self.h)
+#
+#
+# class Square:
+#     def __init__(self, a):
+#         self.a = a
+#
+#     def get_perimetr(self):
+#         return 4 * self.a
+#
+#
+# class Triangle:
+#     def __init__(self, a, b, c):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+#
+#     def get_perimetr(self):
+#         return self.a + self.b + self.c
+#
+#
+# r1 = Rectangle(1, 2)
+# r2 = Rectangle(3, 4)
+# s1 = Square(10)
+# s2 = Square(20)
+# t1 = Triangle(1, 2, 3)
+# t2 = Triangle(4, 5, 6)
+#
+# shape = [r1, r2, s1, s2, t1, t2]
+# for g in shape:
+#     print(g.get_perimetr())
 
-    def __set__(self, instance, value):
-        self.verify_coord(value)
-        # instance.__dict__[self.name] = value
-        setattr(instance, self.name, value)
+# import start
+#
+# start.run()
+
+# from car import electrocar
+#
+#
+# def run():
+#     car1 = electrocar.ElectroCar('Tesla', 'T', 2018, 99000)
+#     car1.show_car()
+#     car1.description_battery()
+#
+#
+# if __name__ == '__main__':
+#     run()
 
 
-class Point3D:
-    x = Integer()
-    y = Integer()
-    z = Integer()
+# Упаковка данных (Сериализация)
+# Распаковка данных (Десериализация)
 
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+# marshal (.pyc)
+# pickle
+# json
 
 
-p1 = Point3D(1, 2, 3)
-print(p1.y)
-p1.z = 100
-print(p1.__dict__)
+import pickle
+
+# file_name = 'basket.txt'
+#
+# shop_list = {
+#     "фрукты": ["яблоки", "манго"],
+#     "овощи": 'морковь',
+#     "бюджет": 1000
+# }
+#
+# with open(file_name, "wb") as fh:
+#     pickle.dump(shop_list, fh)
+#
+# with open(file_name, 'rb') as fh:
+#     shop_list2 = pickle.load(fh)
+#
+# print(shop_list2)
+
+
+# class Test:
+#     num = 35
+#     st = 'Привет'
+#     lst = [1, 2, 3]
+#     d = {'first': "a", "second": 2}
+#     tpl = (22, 36)
+#
+#     def __str__(self):
+#         return f'Число: {Test.num}\nСтрока: {Test.st}\nСписок: {Test.lst}\nСловарь: {Test.d}\nКортеж: {Test.tpl}'
+#
+#
+# obj = Test()
+#
+# my_obj = pickle.dumps(obj)
+# print(my_obj)
+#
+# new_obj = pickle.loads(my_obj)
+# print(new_obj)
+
+
+# class Test2:
+#     def __init__(self):
+#         self.a = 35
+#         self.b = "test"
+#         self.c = lambda x: x + x
+#
+#     def __str__(self):
+#         return f"{self.a} {self.b} {self.c(2)}"
+#
+#     def __getstate__(self):
+#         attr = self.__dict__.copy()
+#         del attr['c']
+#         return attr
+#
+#     def __setstate__(self, state):
+#         self.__dict__ = state
+#         self.c = lambda x: x + x
+#
+#
+# item1 = Test2()
+# item2 = pickle.dumps(item1)
+# item3 = pickle.loads(item2)
+# print(item3.__dict__)
+# print(item3)
+
+
+# data = {
+#     'name': 'Olga',
+#     'age': 35,
+#     True: 1,
+#     'hobbies': ('running', 'singing'),
+#     'children': [
+#         {
+#             'firstName': 'Alice',
+#             'age': 6
+#         }
+#     ]
+# }
+
+# with open("date.json", "w") as fw:
+#     json.dump(data, fw, indent=4)
+#
+# with open("date.json", "r") as fw:
+#     data1 = json.load(fw)
+#
+# print(data1)
+
+# st = json.dumps(data)
+# print(st)
+#
+# data2 = json.loads(st)
+# print(data2)
+
+# x = {
+#     "name": "Виктор"
+# }
+# y = {
+#     "name": "Виктор"
+# }
+# print(json.dumps(x))
+# print(json.dumps(y, ensure_ascii=False))
+
+
+import json
+from random import choice
+
+
+def get_person():
+    name = ''
+    tel = ''
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+    while len(name) != 7:
+        name += choice(letters)
+    # print(name)
+
+    while len(tel) != 10:
+        tel += choice(nums)
+    # print(tel)
+
+    person = {
+        'name': name,
+        'tel': tel
+    }
+    return person
+
+
+def write_json(person_dict):
+    try:
+        data = json.load(open('person.json'))
+    except FileNotFoundError:
+        data = []
+
+    data.append(person_dict)
+    with open('person.json', 'w') as f:
+        json.dump(data, f, indent=2)
+
+
+for i in range(5):
+    write_json(get_person())
+
+
+
+
