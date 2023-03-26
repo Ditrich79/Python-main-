58c16e11ad83b9f7aedc5dd62c80935dd54c563c
Ditrich79 <adk18@mail.ru>
2023-03-26 19:30:54 +0300

    stepik

--- homeworks/stepik.py
+++ homeworks/stepik.py
@@ -931,3 +931,171 @@
 #     result[num] = result.get(num, 0) + 1
 # print(result)
 
+# users = [{'name': 'Todd', 'phone': '551-1414', 'email': 'todd@gmail.com'},
+#          {'name': 'Helga', 'phone': '555-1618', 'email': 'helga@mail.net'},
+#          {'name': 'Olivia', 'phone': '449-3141', 'email': ''},
+#          {'name': 'LJ', 'phone': '555-2718', 'email': 'lj@gmail.net'},
+#          {'name': 'Ruslan', 'phone': '422-145-9098', 'email': 'rus-lan.cha@yandex.ru'},
+#          {'name': 'John', 'phone': '233-421-32', 'email': ''},
+#          {'name': 'Lara', 'phone': '+7998-676-2532', 'email': 'g.lara89@gmail.com'},
+#          {'name': 'Alina', 'phone': '+7948-799-2434', 'email': 'ali.ch.b@gmail.com'},
+#          {'name': 'Robert', 'phone': '420-2011', 'email': ''},
+#          {'name': 'Riyad', 'phone': '128-8890-128', 'email': 'r.mahrez@mail.net'},
+#          {'name': 'Khabib', 'phone': '+7995-600-9080', 'email': 'kh.nurmag@gmail.com'},
+#          {'name': 'Olga', 'phone': '6449-314-1213', 'email': ''},
+#          {'name': 'Roman', 'phone': '+7459-145-8059', 'email': 'roma988@mail.ru'},
+#          {'name': 'Maria', 'phone': '12-129-3148', 'email': 'm.sharapova@gmail.com'},
+#          {'name': 'Fedor', 'phone': '+7445-341-0545', 'email': ''},
+#          {'name': 'Tim', 'phone': '242-449-3141', 'email': 'timm.ggg@yandex.ru'}]
+# users2 = []
+# for i in users:
+#     if i['phone'][-1] == '8':
+#         users2.append(i['name'])
+# print(*sorted(users2))
+#
+# print(*sorted([x['name'] for x in users if x['phone'][-1] == '8']))
+
+# users = [{'name': 'Todd', 'phone': '551-1414', 'email': 'todd@gmail.com'},
+#          {'name': 'Helga', 'phone': '555-1618'},
+#          {'name': 'Olivia', 'phone': '449-3141', 'email': ''},
+#          {'name': 'LJ', 'phone': '555-2718', 'email': 'lj@gmail.net'},
+#          {'name': 'Ruslan', 'phone': '422-145-9098', 'email': 'rus-lan.cha@yandex.ru'},
+#          {'name': 'John', 'phone': '233-421-32', 'email': ''},
+#          {'name': 'Lara', 'phone': '+7998-676-2532', 'email': 'g.lara89@gmail.com'},
+#          {'name': 'Alina', 'phone': '+7948-799-2434'},
+#          {'name': 'Robert', 'phone': '420-2011', 'email': ''},
+#          {'name': 'Riyad', 'phone': '128-8890-128', 'email': 'r.mahrez@mail.net'},
+#          {'name': 'Khabib', 'phone': '+7995-600-9080', 'email': 'kh.nurmag@gmail.com'},
+#          {'name': 'Olga', 'phone': '6449-314-1213', 'email': ''},
+#          {'name': 'Roman', 'phone': '+7459-145-8059'},
+#          {'name': 'Maria', 'phone': '12-129-3148', 'email': 'm.sharapova@gmail.com'},
+#          {'name': 'Fedor', 'phone': '+7445-341-0545', 'email': ''},
+#          {'name': 'Tim', 'phone': '242-449-3141', 'email': 'timm.ggg@yandex.ru'}]
+# users2 = []
+# for i in users:
+#     if 'email' not in i or i['email'] == '':
+#         users2.append(i['name'])
+# print(*sorted(users2))
+
+# di_ct = {
+#     "0": "zero",
+#     "1": "one",
+#     "2": "two",
+#     "3": "three",
+#     "4": "four",
+#     "5": "five",
+#     "6": "six",
+#     "7": "seven",
+#     "8": "eight",
+#     "9": "nine"
+# }
+# for i in input():
+#     print(di_ct[i], end=' ')
+
+# d = {
+#     "CS101": "3004, Хайнс, 8:00",
+#     "CS102": "4501, Альварадо, 9:00",
+#     "CS103": "6755, Рич, 10:00",
+#     "NT110": "1244, Берк, 11:00",
+#     "CM241": "1411, Ли, 13:00"
+# }
+#
+# a = input()
+# if a in d:
+#     print(f'{a}: {d[a]}')
+
+# d = {
+#     "1": [".", ",", "?", "!", ":"],
+#     "2": ["A", "B", "C"],
+#     "3": ["D", "E", "F"],
+#     "4": ["G", "H", "I"],
+#     "5": ["J", "K", "L"],
+#     "6": ["M", "N", "O"],
+#     "7": ["P", "Q", "R", "S"],
+#     "8": ["T", "U", "V"],
+#     "9": ["W", "X", "Y", "Z"],
+#     "0": [" "]
+# }
+# string = input().upper().replace('"', '')
+# for char in string:
+#     for key, value in d.items():
+#         if char in value:
+#             print(key * (value.index(char) + 1), end='')
+
+# def morse_dictionary():
+#     letters = [c for c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789']
+#     morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---',
+#              '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', '-----', '.----', '..---',
+#              '...--', '....-', '.....', '-....', '--...', '---..', '----.']
+#
+#     dictionary = dict(zip(letters, morse))
+#     word = input('Введите строку: ').upper()
+#     for char in word:
+#         for key, value in dictionary.items():
+#             if char == key:
+#                 print(value, end=' ')
+#
+#
+# morse_dictionary()
+
+# result = {}
+# a = [i for i in range(1, 16)]
+# b = [i ** 2 for i in range(1, 16)]
+# c = dict(zip(a, b))
+# result.update(c)
+# print(result)
+
+# dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+# dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+#
+# result = {}
+# result.update(dict1)
+# for key, value in dict2.items():
+#     result[key] = result.get(key, 0) + value
+
+# text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
+#
+# result = {}
+# for char in text:
+#     result[char] = result.get(char, 0) + 1
+# print(result)
+
+
+# s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange ' \
+#     'barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry ' \
+#     'apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana ' \
+#     'melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry ' \
+#     'apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana ' \
+#     'grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit ' \
+#     'pomegranate barley'
+# lst = s.split()
+# result = dict()
+# result2 = dict()
+# for word in lst:
+#     result[word] = result.get(word, 0) + 1
+# biggest_num = max(result.values())
+# for key, value in result.items():
+#     if value == biggest_num:
+#         result2[key] = result2.get(key, value)
+# print(min(result2))
+
+# pets = [('Hatiko', 'Parker', 'Wilson', 50),
+#         ('Rusty', 'Josh', 'King', 25),
+#         ('Fido', 'John', 'Smith', 28),
+#         ('Butch', 'Jake', 'Smirnoff', 18),
+#         ('Odi', 'Emma', 'Wright', 18),
+#         ('Balto', 'Josh', 'King', 25),
+#         ('Barry', 'Josh', 'King', 25),
+#         ('Snape', 'Hannah', 'Taylor', 40),
+#         ('Horry', 'Martha', 'Robinson', 73),
+#         ('Giro', 'Alex', 'Martinez', 65),
+#         ('Zooma', 'Simon', 'Nevel', 32),
+#         ('Lassie', 'Josh', 'King', 25),
+#         ('Chase', 'Martha', 'Robinson', 73),
+#         ('Ace', 'Martha', 'Williams', 38),
+#         ('Rocky', 'Simon', 'Nevel', 32)]
+# result = {}
+#
+# for p, *name in pets:
+#     result.setdefault(tuple(name), []).append(p)
+# print(result)
