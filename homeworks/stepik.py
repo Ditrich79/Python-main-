721d7f826da5f3ff0d515a93bd671f700bf9e3e5
Ditrich79 <adk18@mail.ru>
2023-05-22 22:13:14 +0300

    220523

--- homeworks/stepik.py
+++ homeworks/stepik.py
@@ -2316,3 +2316,10 @@
 #
 # result = sum(map(pow, listA, listB))
 # print(result)
+
+
+# words1 = ['яблоко', 'ананас', 'апельсин', 'хурма', 'гранат', 'мандарин', 'айва']
+# words2 = ['林檎', 'パイナップル', 'オレンジ', '柿']
+# words3 = ['apple', 'pineapple', 'orange', 'persimmon', 'pomegranate']
+#
+# print(len(list(zip(words1, words2, words3))))
