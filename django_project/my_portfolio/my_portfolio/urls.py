b6ad7b11e031611eb47fc2beb49c57ccc726022d
Ditrich79 <adk18@mail.ru>
2023-06-10 20:21:06 +0300

    hw100623

--- django_project/my_portfolio/my_portfolio/urls.py
+++ django_project/my_portfolio/my_portfolio/urls.py
@@ -23,6 +23,10 @@ from skills import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('home', views.home, name='home'),
+    path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
