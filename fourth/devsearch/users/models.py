accbdb1a534cdce93a1683e2df5f0f7f9b89628c
Ditrich79 <adk18@mail.ru>
2023-07-10 22:08:13 +0300

    100723

--- fourth/devsearch/users/models.py
+++ fourth/devsearch/users/models.py
@@ -28,3 +28,19 @@ class Skill(models.Model):
     def __str__(self):
         return self.name
 
+
+class Message(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.SET_NULL, blank=True, null=True)
+    recipient = models.ForeignKey(Profile, on_delete=models.SET_NULL, blank=True, null=True, related_name='messages')
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    subject = models.CharField(max_length=200, blank=True, null=True)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        ordering = ['is_read', '-created']
